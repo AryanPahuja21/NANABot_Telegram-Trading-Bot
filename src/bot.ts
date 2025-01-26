3aea93f75d91452ea3efba956f0742a35e0197f5
AryanPahuja21 <aryanpahuja1421@gmail.com>
2025-01-26 16:58:04 +0530

    dotenv fix

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,13 @@
 import TelegramBot from "node-telegram-bot-api";
+import dotenv from "dotenv";
+dotenv.config();
 
 const token = process.env.TELEGRAM_BOT_TOKEN!;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", (msg: any) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Welcome to the bot!");
+  bot.sendMessage(chatId, "Welcome to NANAbot!");
 });
+
+export default bot;
