0b9d173cf057d72912b4c8740e68238f1e71dd4e
AryanPahuja21 <aryanpahuja1421@gmail.com>
2025-01-27 17:10:36 +0530

    db config

--- src/bot.ts
+++ src/bot.ts
@@ -2,10 +2,21 @@ import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 
 import { start } from "./commands/start";
+import connectDB from "./config/db";
 
 dotenv.config();
 
 const token = process.env.TELEGRAM_BOT_TOKEN!;
 const bot = new TelegramBot(token, { polling: true });
+
+(async () => {
+  try {
+    await connectDB();
+    console.log("Database connected successfully!!");
+  } catch (error) {
+    console.error("Failed to connect database:", error);
+    process.exit(1);
+  }
+})();
 
 bot.onText(/\/start/, (msg) => start(bot, msg));
